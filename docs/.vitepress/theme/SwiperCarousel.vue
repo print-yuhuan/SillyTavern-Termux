bf4ad02f7525fdde3d85ad7a3f56a3ed3a7c1975
print-yuhuan <print.yuhuan@gmail.com>
2025-07-27 09:59:53 +0800

    Updates

---
+++ docs/.vitepress/theme/SwiperCarousel.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="my-swiper">
+    <swiper
+      :modules="[Autoplay, Pagination]"
+      :loop="true"
+      :autoplay="{ delay: 3000, disableOnInteraction: false }"
+      :pagination="{ clickable: true }"
+      :slides-per-view="1"
+      :space-between="20"
+      style="width: 360px; max-width: 90vw;"
+    >
+      <swiper-slide v-for="(img, i) in images" :key="i">
+        <img :src="img.src" :alt="img.alt" style="width:100%;border-radius:22px;box-shadow:0 4px 18px #eee;" />
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script setup>
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Autoplay, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const images = [
+  { src: '/preview1.jpg', alt: '预览1' },
+  { src: '/preview2.jpg', alt: '预览2' },
+  { src: '/preview3.jpg', alt: '预览3' },
+  { src: '/preview4.jpg', alt: '预览4' },
+  { src: '/preview5.jpg', alt: '预览5' },
+  { src: '/preview6.jpg', alt: '预览6' },
+];
+</script>
+
+<style scoped>
+.my-swiper {
+  display: flex;
+  justify-content: center;
+}
+</style>
