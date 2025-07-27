2e9d4bf5833c08997084ca0d79e124b5ef55a234
print-yuhuan <print.yuhuan@gmail.com>
2025-07-27 11:15:15 +0800

    修复已知问题，优化使用体验。

--- release.sh
+++ release.sh
@@ -1,3 +1,20 @@
+cd "$HOME"
+
+ln -sf /data/data/com.termux/files/usr/etc/termux/mirrors/europe/packages.termux.dev \
+      /data/data/com.termux/files/usr/etc/termux/chosen_mirrors
+
+pkg update && pkg upgrade -y -o Dpkg::Options::="--force-confnew"
+
+pkg install -y git nodejs
+
+git clone https://github.com/print-yuhuan/SillyTavern-Termux.git
+
+cd SillyTavern-Termux
+
+npm install
+
+npm run docs:dev
+
 # 发布到 GitHub Pages：
 npm run docs:build:gh
 npm run docs:deploy
